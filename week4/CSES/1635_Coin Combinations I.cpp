29a33e1b832f62dbe3114869609f1cf652393acf
mahathi08 <chinthalamahathi02@gmail.com>
2025-06-26 22:18:10 +0530

    Create 1635_Coin Combinations I.cpp

---
+++ week4/CSES/1635_Coin Combinations I.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD=1e9+7;
+int main() {
+    int n,x;
+    cin>>n>>x;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    
+    vector<int> dp(x+1,0);
+    dp[0]=1;
+    for(int i=1;i<=x;i++){
+        for(int c:v){
+            if(i>=c && dp[i-c] != 0) dp[i]=(dp[i]+dp[i-c])%MOD;
+        }
+    }
+    cout <<  dp[x];
+}
+ 
