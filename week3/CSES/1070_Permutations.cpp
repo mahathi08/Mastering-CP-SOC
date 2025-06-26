3395215c10bba2521982356e48a11e3ca77be1f9
mahathi08 <chinthalamahathi02@gmail.com>
2025-06-26 22:04:00 +0530

    Update 1070_Permutations.cpp

--- week3/CSES/1070_Permutations.cpp
+++ week3/CSES/1070_Permutations.cpp
@@ -1,1 +1,31 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n==1) cout<<1;
+    else if(n<=3)cout<<"NO SOLUTION";
+    else{
+        vector<int> v(n);
+        if(n%2){
+            for(int i=0;i<n;i+=2){
+                v[i]=(i/2)+1;
+            }
+            for(int i=1;i<n;i+=2){
+                v[i]=(i+3+n)/2;
+        }}
+        else{
+            for(int i=0;i<n;i+=2){
+                v[i]=(n+i+2)/2;
+            }
+            for(int i=1;i<n;i+=2){
+                v[i]=(i+1)/2;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            cout<<v[i]<<" ";
+        }
+    }
+    
+}
