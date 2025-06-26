5753b9bb1c3bd2145f699960ce84cdf52724f5ee
mahathi08 <chinthalamahathi02@gmail.com>
2025-06-26 22:02:34 +0530

    Create 1079_Binomial Coefficients.cpp

---
+++ week3/CSES/1079_Binomial Coefficients.cpp
@@ -0,0 +1,52 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+const int MOD = 1e9 + 7;
+const int MAX = 1e6 + 10;
+ 
+vector<long long> fact(MAX), inv_fact(MAX);
+ 
+long long modpow(long long a, long long b) {
+    long long res = 1;
+    a %= MOD;
+    while (b > 0) {
+        if (b & 1) res = res * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void precompute() {
+    fact[0] = 1;
+    for (int i = 1; i < MAX; ++i) {
+        fact[i] = fact[i - 1] * i % MOD;
+    }
+ 
+    inv_fact[MAX - 1] = modpow(fact[MAX - 1], MOD - 2);
+    for (int i = MAX - 2; i >= 0; --i) {
+        inv_fact[i] = inv_fact[i + 1] * (i + 1) % MOD;
+    }
+}
+ 
+long long binomial(int a, int b) {
+    if (b < 0 || b > a) return 0;
+    return fact[a] * inv_fact[b] % MOD * inv_fact[a - b] % MOD;
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    precompute();
+ 
+    int n;
+    cin >> n;
+    while (n--) {
+        int a, b;
+        cin >> a >> b;
+        cout << binomial(a, b) << '\n';
+    }
+ 
+    return 0;
+}
